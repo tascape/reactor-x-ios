8bd20588149e13db80b62de7a170f7c504f37846
Linsong Wang <tascape@gmail.com>
2016-07-20 22:03:17 -0700

    update test case with task case

--- uia-test/src/main/java/com/tascape/reactor/ios/task/UiAutomationTest.java
+++ uia-test/src/main/java/com/tascape/reactor/ios/task/UiAutomationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.tascape.reactor.ios.test;
+package com.tascape.reactor.ios.task;
 
 import com.tascape.reactor.ios.driver.App;
 
